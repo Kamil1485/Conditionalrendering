39cc1ffa8c17c4f7b28ce7d190e05f18b8e661c2
Kamil1485 <81561240+Kamil1485@users.noreply.github.com>
2023-04-16 12:18:17 +0300

    Update Ternary.js

--- conditionalRendering-6/src/Ternary.js
+++ conditionalRendering-6/src/Ternary.js
@@ -1,20 +1,5 @@
 import React from 'react'
-//3- Vejeteryansa ismini ve vejeteryan diyor
-
-
-/*
- if (isVegetarian) {
-    return 'tofu';
-  } else {
-    return 'fish';
-  }
-*/
-//^ Person-Recipe-Ternanary  son ekrana basma kısmı burası artık !! Person Recipei cagırıoyr recipe de ternanry basacak tabi kendi icindekileride basıyor ekstra bunu cagırıyor
-
 function Ternary({isim,Vejeteryanmı}) {
-  //alternatif Nested conditionlar icin yeniden tanımlama  if else i
 return Vejeteryanmı ? `${isim} Vejeyeryan Et yemez `:`${isim} Vejeteryan degil Et yiyebilir`              
-
 }
-
 export default Ternary
